62f07d52e746d56eefe6eaada5a7e67755e5ea14
ben-laird <86321957+ben-laird@users.noreply.github.com>
2022-06-17 13:24:09 -0500

    Refactor App file to be named MyApp

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // Import the React and ReactDOM libs
 import React from "react";
 import ReactDOM from "react-dom";
-import MyApp from "./App";
+import MyApp from "./MyApp";
 
 // Take the react component and show it on the screen
 ReactDOM.render(<MyApp />, document.querySelector("#root"));
