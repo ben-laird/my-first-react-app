984942e97e5fddc0104ad93385703353e035a453
ben-laird <86321957+ben-laird@users.noreply.github.com>
2022-06-17 13:30:14 -0500

    Begin using TSX for MyApp

--- src/MyApp.tsx
+++ src/MyApp.tsx
@@ -1,3 +1,5 @@
+import * as React from "react";
+
 const MyApp = () => {
   const buttonText = { text: "click me!" };
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // Import the React and ReactDOM libs
 import React from "react";
 import ReactDOM from "react-dom";
-import MyApp from "./MyApp";
+import MyApp from "./MyApp.tsx";
 
 // Take the react component and show it on the screen
 ReactDOM.render(<MyApp />, document.querySelector("#root"));
